a0df6e43bb37233eb175a04f9b50f099a08c73c5
Aurore <aurore.enoch@gmail.com>
2022-12-21 13:46:49 +0100

    Header & Footer Responsive
    Made the responsive for the header and footer

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
 import './scss/app.scss';
+import script from './script/script.js';
 
 // start the Stimulus application
 import './bootstrap';
