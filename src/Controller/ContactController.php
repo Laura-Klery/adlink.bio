c1f8a4108db56178a19a2ebd0ed2046065eab41d
Lolibeli <119513459+Lolibeli@users.noreply.github.com>
2022-12-22 11:49:43 +0100

    Pages légales plus page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,18 +1,24 @@
 <?php
 
-namespace App\Controller;
-
+//namespace App\Controller;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
+
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function addContact() 
     {
-        return $this->render('contact/index.html.twig');
+        $contact = new Contact();
+        
+        $form = $this->createForm(ContactType::class, $contact);
+        return $this->render('contact/index.html.twig',[
+            'form' => $form->createView()
+        ]);
     }
 }
