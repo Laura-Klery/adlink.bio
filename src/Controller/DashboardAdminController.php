f3a7493f67b540e21201c3fc37e7dc7a90d0d375
Aurore <aurore.enoch@gmail.com>
2023-01-24 20:54:50 +0100

    Dashboard Admin Modifications
    added the get for users emails

--- src/Controller/DashboardAdminController.php
+++ src/Controller/DashboardAdminController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Repository\UserRepository;
 
 class DashboardAdminController extends AbstractController
 {
     /**
      * @Route("/admin_dashboard", name="admin_dashboard")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('dashboard_admin/index.html.twig');
+        $users = $userRepository -> findAll();
+        //var_dump($users); //LOG
+        return $this->render('dashboard_admin/index.html.twig', [
+            'users' => $users
+        ]);
     }
 }
