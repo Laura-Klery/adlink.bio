159b6f1991d4d4bdbca1edaffa8d6b28418dbfe7
Laura-Klery <lklery@yahoo.fr>
2023-01-24 21:05:32 +0100

    Dashboard User - add functions

--- src/Controller/DashboardUserController.php
+++ src/Controller/DashboardUserController.php
@@ -2,25 +2,44 @@
 
 namespace App\Controller;
 
+use App\Form\BrandFormType;
 use App\Form\InformationFormType;
 use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DashboardUserController extends AbstractController
 {
     /**
      * @Route("/dashboard_user", name="dashboard_user")
      */
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, ManagerRegistry $doctrine, Request $request): Response
     {
-        $user = $userRepository->find($this->getUser());
+        $user = $this->getUser();
         $informationsAccount = $this->createForm(InformationFormType::class, $user);
+        $informationsAccount->handleRequest($request);
+        if($informationsAccount->isSubmitted() && $informationsAccount->isValid()) {
+          $em = $doctrine->getManager();
+          $passwordNew = $informationsAccount->get('plainPassword')->getData();
+          $hashPasswordNew = $userNewPassword->hashPassword($user, $passwordNew);
+          $user->setPassword($hashPasswordNew);
+          $em->flush();
+        }
+
+
+        $brand = $this->createForm(BrandFormType::class);
+
+
+
+
 
         return $this->render('dashboard_user/index.html.twig', [
+            'user' => $user,
             'account' => $informationsAccount->createView(),
-            'user' => $user
+            'brand' => $brand->createView(),
         ]);
     }
 
